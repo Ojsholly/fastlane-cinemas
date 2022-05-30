dcd69fa5e1593a73837300753e19dd3a3ccf01e4
Olusola Ojewunmi <olusolaojewunmi@gmail.com>
2022-05-30 11:09:10 +0100

    Added database schema and model entity relationships.

---
+++ app/Models/Schedule.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use BinaryCabin\LaravelUUID\Traits\HasUUID;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Schedule extends Model
+{
+    use HasFactory, HasUUID;
+
+    protected $fillable = ['movie_id', 'branch_id', 'start_at'];
+
+    public function movies(): BelongsTo
+    {
+        return $this->belongsTo(Movie::class, 'movie_id', 'uuid');
+    }
+
+    public function branches(): BelongsTo
+    {
+        return $this->belongsTo(Branch::class, 'branch_id', 'uuid');
+    }
+}

5c22da89dbd0b32126ce9cba2f6ad71ee72897b5
Olusola Ojewunmi <olusolaojewunmi@gmail.com>
2022-05-30 14:23:32 +0100

    Added cinema index page.

--- app/Http/Controllers/API/FetchBranchesController.php
+++ app/Http/Controllers/API/FetchBranchesController.php
@@ -5,17 +5,20 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Branch\BranchResourceCollection;
 use App\Repositories\BranchRepository;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class FetchBranchesController extends Controller
 {
     /**
      * Handle the incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param BranchRepository $branchRepository
+     * @return JsonResponse
      */
-    public function __invoke(Request $request, BranchRepository $branchRepository)
+    public function __invoke(Request $request, BranchRepository $branchRepository): JsonResponse
     {
         $branches = $branchRepository->getAllBranches([], ['schedules']);
 
